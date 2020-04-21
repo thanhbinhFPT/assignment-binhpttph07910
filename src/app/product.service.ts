31093e08a7b92d0fc2f419e153871522db3c7c9c
thanhbinhFPT <phamthithanhbinh20@gmail.com>
2020-04-21 14:21:47 +0700

    2 files modified

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Data } from './mockData';
 import { Product } from './product';
 import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
