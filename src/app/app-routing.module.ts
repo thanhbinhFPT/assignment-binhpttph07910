28bc93f128c5c9f0559e7f34ddd60fca4e1c6a59
thanhbinhFPT <phamthithanhbinh20@gmail.com>
2020-04-21 12:38:23 +0700

    2 files modified, 12 added, 14 deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AdminTextComponent } from './admin/admin-text/admin-text.component';
-import { DashboardComponent } from './admin/dashboard/dashboard.component';
-import { ProductManagerComponent } from './product-manager/product-manager.component';
-import { AddProductComponent } from './add-product/add-product.component';
 
 
 
@@ -12,16 +7,7 @@ import { AddProductComponent } from './add-product/add-product.component';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'admin', component: AdminTextComponent,
-children: [
-  {path: '', redirectTo: 'dashboard', pathMatch:'full'},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'product', component: ProductManagerComponent},
-  {path: 'product/:ProductID', component:ProductManagerComponent},
-  { path: 'product/add', component: AddProductComponent},
-]}
+
 ];
 
 @NgModule({
